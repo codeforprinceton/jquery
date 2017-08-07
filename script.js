354d7e86fb01b992aeb6dbc5f421742f4fd2206f
Shaun Ellis <shaun@sdellis.com>
2017-08-07 17:54:52 -0400

    fetch and render comments

--- script.js
+++ script.js
@@ -17,38 +17,41 @@ function assetEndpoint(asset_id) {
 }
 
 client.getSpace(space_id)
-.then((space) => space.getEntries(query))
+.then((space) => space.getEntries({ content_type: '1xYw5JsIecuGE68mmGMg20' }))
 .then((response) => renderPhotos(response.items))
 .catch(console.error)
 
 function renderPhotos (data) {
   console.log(data)
   window.data = data
-  $.each( data, function( i, photo ){
+  $.each( data, function( i, item ){
     var img_url = "https:"
-    $.getJSON( assetEndpoint(photo.fields.photo['en-US'].sys.id), { access_token: accessToken, w: '200', h: '150', fit: 'fill' } )
-      .done(function( data ) {
-        img_url = img_url + data.fields.file[lang].url
-        var $div = $("<div>", {id: photo.fields.slug[lang], "class": "col-md-3 col-sm-6"})
-        var $buttons_div = $("<div>", {"class": "likes"})
-        var $p = $("<p></p>")
-        var $like_button = $('<button type="button" id="' + photo.sys.id + '" class="btn btn-primary btn-sm like-button" value="' + photo.fields.likes[lang] +
-                            '"><span class="glyphicon glyphicon-heart"></span> <span class="likenum">' + photo.fields.likes[lang] + '</span></button>')
-        var $comment_button = $('<button type="button" class="btn btn-default btn-sm" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-comment"></span> Comment</button>')
-        var $title = $("<h2>" + photo.fields.title[lang] + "</h2>")
-        var $img = $("<img>", {"class": "img-thumbnail"})
-        $p.append($like_button,$comment_button)
-        $buttons_div.append($p)
-        $img.attr( "src", img_url )
-        $div.append($title)
-        $div.append($img)
-        $div.append($buttons_div)
-        $("#photo-row").append($div)
-      })
-      .fail(function( jqxhr, textStatus, error ) {
-        var err = textStatus + ", " + error;
-        console.log( "Request Failed: " + err );
-    });
+    if( item.fields.hasOwnProperty('photo') ){
+      var photo = item
+      $.getJSON( assetEndpoint(photo.fields.photo[lang].sys.id), { access_token: accessToken, w: '200', h: '150', fit: 'fill' } )
+        .done(function( data ) {
+          img_url = img_url + data.fields.file[lang].url
+          var $div = $("<div>", {id: photo.fields.slug[lang], "class": "col-md-3 col-sm-6"})
+          var $buttons_div = $("<div>", {"class": "likes"})
+          var $p = $("<p></p>")
+          var $like_button = $('<button type="button" id="' + photo.sys.id + '" class="btn btn-primary btn-sm like-button" value="' + photo.fields.likes[lang] +
+                              '"><span class="glyphicon glyphicon-heart"></span> <span class="likenum">' + photo.fields.likes[lang] + '</span></button>')
+          var $comment_button = $('<button type="button" id="' + photo.sys.id + '" class="btn btn-default btn-sm comment-button" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-comment"></span> Comment</button>')
+          var $title = $("<h2>" + photo.fields.title[lang] + "</h2>")
+          var $img = $("<img>", {"class": "img-thumbnail"})
+          $p.append($like_button,$comment_button)
+          $buttons_div.append($p)
+          $img.attr( "src", img_url )
+          $div.append($title)
+          $div.append($img)
+          $div.append($buttons_div)
+          $("#photo-row").append($div)
+        })
+        .fail(function( jqxhr, textStatus, error ) {
+          var err = textStatus + ", " + error;
+          console.log( "Request Failed: " + err );
+      });
+    }
   });
 }
 
@@ -67,3 +70,28 @@ $(document).on("click", ".like-button", function(){
   })
   .catch(console.error)
 });
+
+//Get Comments
+$(document).on("click", ".comment-button", function(){
+  $('p.comments').empty()
+  client.getSpace(space_id)
+  .then((space) => space.getEntries({
+    content_type: 'remark',
+    'fields.commentImage.sys.id': this.id
+  }))
+  .then((response) => renderComments(response.items))
+  .catch(console.error)
+});
+
+//Render Comments
+function renderComments (data) {
+  if( data.length > 0 ) {
+    $.each( data, function( i, comment ){
+      var $comment = $("<div class='well well-sm'>" + comment.fields.commentText[lang] + "</div>")
+      $('p.comments').append($comment)
+    });
+  }else{
+    var $comment = $("<div>Be the first to comment!</div>")
+    $('p.comments').append($comment)
+  }
+}
